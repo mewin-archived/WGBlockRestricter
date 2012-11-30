26a863fbf4b9cfd893f945c8c07a7e1ddf968a6b
mewin <mewin001@hotmail.de>
2012-11-30 17:53:51 +0100

    added a useless class

---
+++ com/mewin/WGBlockRestricter/UselessClass.java
@@ -0,0 +1,9 @@
+package com.mewin.WGBlockRestricter;
+
+/**
+ *
+ * @author mewin<mewin001@hotmail.de>
+ */
+public class UselessClass {
+    //THIS CLASS IS COMPLETELY USELESS, I JUST CREATED IT, BECAUSE BUKKIT.ORG DOESN'T LET ME UPLOAD THE ARCHIVE
+}
